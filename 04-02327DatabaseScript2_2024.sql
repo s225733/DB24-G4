94fa2f1f35bac47553892fb1babb0ab7b444c012
Adrian Macauley (s225733) <S225733@dtu.dk>
2024-11-16 16:08:14 +0100

    Added additional display options in Script 2 and another INSERT.

--- 04-02327DatabaseScript2_2024.sql
+++ 04-02327DatabaseScript2_2024.sql
@@ -8,8 +8,24 @@ SET
     country = 'Updated Country'             -- New country
 WHERE 
     first_name = 'John' AND 
-    last_name = 'Doe';                      -- Targeting John Doe
+    last_name = 'Daisy';                      -- Targeting John Doe
 
+DELETE FROM Passenger
+WHERE Id = 'CIT01';
+
+INSERT INTO Busline Values('Line 43','City Square','Central Station' );
+
+-- Display all passengers
 SELECT * FROM Passenger;
-SELECT * FROM Stop;
+
+-- Display all journeys
 SELECT * FROM Journey;
+
+-- Display all stops
+SELECT * FROM Stop;
+
+-- Display all buslines
+SELECT * FROM Busline;
+
+-- Display all orders of stop
+SELECT * FROM Order_of_Stop;
