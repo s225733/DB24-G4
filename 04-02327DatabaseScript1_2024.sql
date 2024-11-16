cfe22e9a6e6c859687622dc7f3d051e41d21639d
Adrian Macauley (s225733) <S225733@dtu.dk>
2024-11-16 14:38:52 +0100

    Minor adjustment to civic_number and 1 insert statement.

--- 04-02327DatabaseScript1_2024.sql
+++ 04-02327DatabaseScript1_2024.sql
@@ -1,15 +1,20 @@
 -- Create Passenger table if it does not exist
+
+-- CREATE DATABASE mandatory; USE mandatory; 
+-- uncomment above statement when you want to create the schema that is needed. :)
+-- DROP DATABASE IF EXISTS mandatory;
+-- uncomment above statement to delete schema. :)  
 CREATE TABLE IF NOT EXISTS Passenger (
-    Id VARCHAR(20) PRIMARY KEY,
+    Id VARCHAR(10) PRIMARY KEY,
     email_address VARCHAR(255) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100),
     street_name VARCHAR(255) NOT NULL,
-    civic_number INT NOT NULL,
+    civic_number VARCHAR(11) NOT NULL,
     city VARCHAR(100) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL,
+    zip_code VARCHAR(4) NOT NULL,
     country VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(20)
+    phone_number VARCHAR(15)
 );
 
 -- Create Journey table if it does not exist
@@ -47,3 +52,41 @@ CREATE TABLE IF NOT EXISTS Order_of_Stop (
     FOREIGN KEY (stop_id) REFERENCES Stop(Id),
     FOREIGN KEY (journey_id) REFERENCES Journey(Id)
 );
+
+INSERT INTO Passenger(Id, 
+email_adress, 
+first_name, 
+last_name, 
+street_name, 
+civic_number, 
+city, 
+zip_code, 
+country, 
+phone_number)
+Values( 'CIT01','first@email.com','First','Name','Initial','Street','010101-0101','Copenhagen','2100','Denmark','+451234567890');
+
+INSERT INTO Passenger (
+Id,
+email_address,
+first_name,
+last_name,
+street_name,
+civic_number,
+city,
+zip_code,
+country,
+phone_number
+)
+VALUES (
+    'CIT01',                          -- Unique identifier
+    'johndoe@example.com',            -- Email address
+    'John',                           -- First name
+    'Doe',                            -- Last name
+    'Main Street',                    -- Street name
+    '150557-1234',                    -- Civic number in CPR format (DDMMYY-XXXX)
+    'Sample City',                    -- City
+    '1234',                          -- Zip code
+    'Sample Country',                 -- Country
+    '+1234567890'                     -- Phone number
+);
+
